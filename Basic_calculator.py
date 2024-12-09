502044f2d6ec7969b97e280577861d896f5c7cf5
Johnas <johnas1433@yahoo.com>
2024-12-09 14:03:02 -0800

    basic calculator file added

---
+++ Basic_calculator.py
@@ -0,0 +1,11 @@
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+
+number1 = float(number1)   # we used FLOAT because we want our users to be able to use both decimals and whole numbers
+number2 = float(number2)
+
+result = number1 + number2
+
+print(result)
+
+
