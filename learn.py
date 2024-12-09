1bdf6e0f9fd77a78d5282f0040030126b2596bfb
Johnas <johnas1433@yahoo.com>
2024-12-09 12:52:30 -0800

    new file added

---
+++ learn.py
@@ -0,0 +1,26 @@
+# Variables & Data Types
+
+# A variable is a data container which helps us to store, manage and work with data. 
+
+character_name = "John"
+character_age = "35"
+
+# working with STRINGS
+
+print("HakunaMatata AI") # use \n to separate with lines or use \" to add a quoation mark or we can also create a variable with a string
+
+print("HakunaMatata \n Lab")
+print("HakunaMatata\"Lab")
+phrase = "HakunaMatata AI"
+
+print(phrase.upper())
+print(phrase.islower())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("AI"))
+print(phrase.replace("AI", "Lab"))
+
+# working with NUMBERS 
+
+
