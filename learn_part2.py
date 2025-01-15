47485d7554e4ca0a4af26796860aa8e06a0be8b3
Johnas <johnas1433@yahoo.com>
2025-01-15 16:22:40 +0000

    new changes

--- learn_part2.py
+++ learn_part2.py
@@ -1,6 +1,6 @@
 # PART 2 - DATA STRUCTURES
 
-# Opening a File - before we can read the contents of the file we must tell python whihc file we are going to work with and what we will be doing wiht the file. This is done with the: open() function
+# 1. Opening a File - before we can read the contents of the file we must tell python whihc file we are going to work with and what we will be doing wiht the file. This is done with the: open() function
 # Open() returns a "file handle" - a variable used to perform operations on the file
 # Similar to "File -> Open" in a Word Processor 
 # handle = open("filename", "mode(read or write)"") - if we leave without mode then it's read by default
@@ -12,6 +12,83 @@ stuff = "Johnas\nChami"
 stuff 
 print(stuff)
 len(stuff)
+
+# 2. Read a File 
+# A file handle open for read can be treated as a sequence of strings where each line in the file is a string in the sequence 
+# We can use the FOR statement to iterate through a sequence 
+
+xfile = open("mbox.txt")
+for cheese in xfile:
+    print(cheese)
+
+# Counting Lines in a file 
+fhand = open("mbox.txt")
+count = 0
+for line in fhand:
+    count = count + 1 
+print("Line Count:", count)
+
+# Reading the *Whole* File 
+fhand = open("mbox-short.txt")
+inp = fhand.read()
+print(len(inp))
+
+# Searching through a File
+fhand = open("mbox-short.txt")
+for line in fhand:
+    if line.startswith("From:") :
+        print(line)   # here print() will leave a space because of default \n 
+
+# Remove newline with strip() from the string library. Newline is like a whitespace
+fhand = open("mbox-short.txt")
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith("From:") :
+        print(line)  
+
+# Skipping with CONTINUE
+fhand = open("mbox-short.txt")
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith("From:") :
+        continue
+    print(line)
+
+# Using IN to select lines
+fhand = open("mbox-short.txt")
+for line in fhand:
+    line = line.rstrip()
+    if not "uct.ac.za" in line :
+        continue
+    print(line) 
+   
+# Prompt for File Name
+fname = input("Enter the file name: ")
+fhand = open(fname)
+count = 0
+for line in fhand:
+    if line.startswith("Subject: ") :
+        count = count + 1
+print("There were", count, "subject lines in", fname)
+
+# Bad File Names
+fname = input("Enter the file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
+    
+count = 0
+for line in fhand:
+    if line.startswith("Subject: ") :
+        count = count + 1
+print("There were", count, "subject lines in", fname)
+
+
+
+
+
 
 
 
