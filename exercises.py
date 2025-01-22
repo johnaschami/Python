323e8fe1a47ae7e5452a1439ac7c387f984c057c
Johnas <johnas1433@yahoo.com>
2025-01-22 18:03:01 +0000

    qn 5 added

--- exercises.py
+++ exercises.py
@@ -70,5 +70,9 @@ if len(password) == 8:
 # 4. Shopping Discount Calculator (Medium)
 # Create a program that: Asks the user for the total amount of a purchase. Applies a discount based on these conditions: Purchases above $100 get a 10% discount. Purchases above $200 get a 20% discount. Print the discounted price.
 
+# 5. Palindrome Checker for Words (Medium)
+# Write a program to: Ask the user to input a word. Check if it is a palindrome (reads the same forward and backward).  
+
+
 
 
