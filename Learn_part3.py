1d442f2406c01ccd5664bc979f3c3a9cc6f470d2
Johnas <johnas1433@yahoo.com>
2025-04-10 20:36:35 +0100

    new FILE edited

--- Learn_part3.py
+++ Learn_part3.py
@@ -1,1 +1,9 @@
 # USING PYTHON TO ACCESS WEB DATA
+
+# Sockets in Python (Python has a built in support for TCP Scokets)
+
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect( ("data.pr4e.org", 80))
+
+
